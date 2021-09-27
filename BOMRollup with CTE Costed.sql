06814391e2ad6612103031a0d3dbaa636096d311
saadmanzaman@gmail.com <saadmanzaman@gmail.com>
2021-09-27 04:57:41 -0700

    Cost Rollup creation

--- BOMRollup with CTE Costed.sql
+++ BOMRollup with CTE Costed.sql
@@ -1,4 +1,11 @@
-﻿----------------------------------
+﻿/*
+Query will perform BOM rollup on a set of PartNumbers and their Requirements (Parent/Child) relationship.
+  Once it creates the BOM, it will rollup the raw material cost into the parent materials.
+  Ignore labor.
+
+  */
+
+----------------------------------
 --CREATE PARTS LIST WITH PARENT CHILD RELATIONSHIP
 DROP TABLE IF EXISTS #Req;
 CREATE TABLE #Req (ParentPN NVARCHAR(20), ChildPN NVARCHAR(20), Cost DECIMAL(18,2))
@@ -32,6 +39,8 @@ WITH bomcte AS(
         ,0 AS Level
         ,b.ParentPN AS SourcePN
         ,CAST(b.ParentPN AS NVARCHAR(MAX)) AS DemandBranch
+        ,CHECKSUM(CAST(b.ParentPN AS NVARCHAR(MAX))) AS DemandBranchID
+        ,NULL AS ParentBranchID
   FROM #Req b
   WHERE b.ParentPN = 'Major SubAssy 1' --set the PN from the PN list that you are making; use Major SubAssy 1
 
@@ -43,6 +52,8 @@ WITH bomcte AS(
         ,bc.Level + 1 AS Level
         ,bc.SourcePN
         ,CAST(bc.DemandBranch AS NVARCHAR(MAX)) + ' > ' + CAST(b.ParentPN AS NVARCHAR(MAX)) AS DemandBranch
+        ,CHECKSUM(CAST(bc.DemandBranch AS NVARCHAR(MAX)) + ' > ' + CAST(b.ParentPN AS NVARCHAR(MAX))) AS DemandBranchID
+        ,bc.DemandBranchID AS ParentBranchID
   FROM #Req b
   INNER JOIN bomcte bc
     ON bc.ChildPN = b.ParentPN
@@ -53,8 +64,46 @@ SELECT *
   FROM bomcte
  
 ----------------------------------
---SELECT DATA
-
-SELECT *
+--CONDENSE DATA INTO UNIQUE P/N
+DROP TABLE IF EXISTS #BOMFlat;
+SELECT b.ParentPN      
+      ,SUM(b.Cost) AS Cost
+      ,b.Level
+      ,b.SourcePN
+      ,b.DemandBranch
+      ,b.DemandBranchID
+      ,b.ParentBranchID
+  INTO #BOMFlat
   FROM #BOM b
+  GROUP BY b.ParentPN      
+      ,b.Level
+      ,b.SourcePN
+      ,b.DemandBranch
+      ,b.DemandBranchID
+      ,b.ParentBranchID
+  ORDER BY b.Level
+
+  SELECT * FROM #BOMFlat b
+    ORDER BY b.Level
+
+----------------------------------
+--Use While Loop to sum Costs
+
+  CREATE TABLE #Rollup (ParentPN NVARCHAR(20), ChildPN NVARCHAR(20), Cost DECIMAL(18,2))
+
+DECLARE @MaxL AS INT = (SELECT MAX(b.Level) FROM #BOMFlat b)
+DECLARE @Loop AS INT = @MaxL
+
+WHILE @Loop >= 0
+  BEGIN
+
+  SELECT b.ParentPN
+         ,b.ChildPN
+        ,
+    FROM #BOMFlat b
+    WHERE b.Level = @Loop
   
+    SET @Loop = @Loop - 1
+
+  END
+
